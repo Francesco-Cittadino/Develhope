94e020a2d8bfcbfed0939bb6711abc3daa158aa3
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-04-12 19:49:22 +0200

    nine

---
+++ Develhope-JS/es9/exercise-1.js
@@ -0,0 +1,16 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+console.log(Object.keys(person));
+// Crea un oggetto `persona`. Stampa la sua coppia
+// chiave/valore nella console. Prova ad usare 
+// il metodo `Object.kyes`:
+
+// ```
+// nome: Mario
+// cognome: Rossi
+// età: 25
+// ```
+// Print values of person using Object.keys
