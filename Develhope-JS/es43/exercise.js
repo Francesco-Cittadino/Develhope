3790ab4b558a61a4a09a40f816fcc3396557a056
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-05-16 00:00:30 +0200

    ex

---
+++ Develhope-JS/es43/exercise.js
@@ -0,0 +1,14 @@
+// Nel file `exercise.js` troviamo un oggetto `user.
+
+// Scrivi una funzione che salvi l'oggetto utente in `localStorage` 
+// con la sua chiave 'user'.
+
+// Suggerimenti:
+// - Ricordati di usare i `metodi JSON` disponibili (parse, stringify)
+
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+
