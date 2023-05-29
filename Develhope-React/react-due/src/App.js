38d7413cfc4becce1b6b0923e59c025c3fdfefd7
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-05-29 20:51:08 +0200

    react-due

---
+++ Develhope-React/react-due/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './Welcome';
+
+// Crea un componente della classe Welcome 
+// che riceva un oggetto di scena per il nome e visualizzi Welcome, 
+// {name}! messaggio all'interno di un tag p. 
+// Rendi questo componente al tuo componente App, 
+// passandogli un oggetto di nome a tua scelta.
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Welcome name="Francesco Cittadino"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
