1af41942cd14cfec539edcabc5fd8e39c0d64eff
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-05-30 18:05:32 +0200

    react-4

--- Develhope-React/react/src/Welcome.js
+++ Develhope-React/react/src/Welcome.js
@@ -1,8 +1,15 @@
 import React from "react";
-
-class Welcome extends React.Component{
-    render(name){
-        return ( <p>Welcome {name="Francesco Cittadino"}</p>)
+// Modifica il componente Welcome in modo 
+// che riceva un secondo prop chiamato age e lo visualizzi sotto il messaggio 
+// 'welcome', all'interno di un tag p dopo il messaggio "Your age is ".
+class Welcome extends React.Component {
+    render(name) {
+        return (
+            <div>
+                <p>Welcome {name = "Francesco Cittadino"} </p>
+                <p>Your age is {this.props.age}</p>
+            </div>
+        )
     }
 }
 export default Welcome;
