2a8fe946e2f9356284acf3cfd101bad04d78bfa9
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-05-30 17:55:00 +0200

    react-3

---
+++ Develhope-React/react/src/Welcome.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Welcome extends React.Component{
+    render(name){
+        return ( <p>Welcome {name="Francesco Cittadino"}</p>)
+    }
+}
+export default Welcome;
