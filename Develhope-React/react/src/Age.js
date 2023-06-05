4b6fe4b771feca911326858858264738655e4cdf
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-06-05 10:10:49 +0200

    react-8

--- Develhope-React/react/src/Age.js
+++ Develhope-React/react/src/Age.js
@@ -3,7 +3,7 @@ import React from "react";
 class Age extends React.Component {
     render() {
         return (
-                this.props.age > 18 &&  <p>Your age is {this.props.age}</p>
+                this.props.age &&  <p>Your age is {this.props.age}</p>
         )
     }
 
