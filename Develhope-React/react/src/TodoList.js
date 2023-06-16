45760b7db1beb1fd053d3c21cf80ddb16ed83907
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-06-16 19:41:38 +0200

    react

--- Develhope-React/react/src/TodoList.js
+++ Develhope-React/react/src/TodoList.js
@@ -2,47 +2,58 @@ import React from "react";
 
 class TodoList extends React.Component {
     state = {
-        user: ["name", "uno", "tre", "name1", "name2", "name3"],
-        ciao: "",
+        user: ["uno", "due", "tre", "quattro", "cinque"],
+        val: "",
         disable: false,
     }
-    handleChange = (event) => {
-        const name = event.target.name;
+
+    handleChange = (event) => {;
         const value = event.target.value;
         this.setState({
-            ciao: value,
+            val: value,
         })
-        console.log(value)
     }
+
     handleGen = (event) => {
-        // const name = event.target.name;
         const value = event.target.value;
         this.setState({
-            user: [...this.state.user, this.state.ciao],
-            disable: true,
+            user: [...this.state.user, this.state.val],
+            // disable: true,
+            val: "",
         })
-        // console.log(this.state.user)
     }
+
     handleRes = (event) => {
-       const id = event.target.id;
-       
-       
-       this.setState({
-           user: this.state.user.filter((el)=>
-               el !== id
-           ),
-       })
+        const id = event.target.id;
+        this.setState({
+            user: this.state.user.filter((el) =>
+                el !== id
+            ),
+        })
     }
+
     render() {
         return (
             <div>
-                <input name="in" type="text" disabled={this.state.disable} onChange={this.handleChange} value={this.state.ciao} />
-                <input name="genera" type="submit" onClick={this.handleGen} value="add" />
-                
+                <input name="in"
+                    type="text"
+                    // disabled={this.state.disable}
+                    onChange={this.handleChange}
+                    value={this.state.val} />
+                <input name="genera"
+                    type="submit"
+                    onClick={this.handleGen}
+                    value="add" />
                 <ul>
-
-                    {this.state.user.map((el, index) =>
-                        (<li key={index}>{el} <input id={el} name="remove" type="button" onClick={this.handleRes} value="remove" /></li>)
+                    {this.state.user.map((el, index) => (
+                        <li key={index}>
+                            {el}
+                            <input id={el}
+                                name="remove" 
+                                type="button"
+                                onClick={this.handleRes}
+                                value="remove" />
+                        </li>)
                     )}
                 </ul>
 
