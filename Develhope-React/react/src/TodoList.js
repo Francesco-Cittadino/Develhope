7085d227bbf6595f16d4b8270270cb75ebc7fc49
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-06-16 18:16:00 +0200

    react

--- Develhope-React/react/src/TodoList.js
+++ Develhope-React/react/src/TodoList.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 class TodoList extends React.Component {
-    state={
-        user: ["name","uno","tre","name1","name2","name3"],
-        ciao: ""
+    state = {
+        user: ["name", "uno", "tre", "name1", "name2", "name3"],
+        ciao: "",
+        disable: false,
     }
-    handleChange = (event)=>{
+    handleChange = (event) => {
         const name = event.target.name;
         const value = event.target.value;
         this.setState({
@@ -17,22 +18,23 @@ class TodoList extends React.Component {
         // const name = event.target.name;
         const value = event.target.value;
         this.setState({
-            user: [...this.state.user, this.state.ciao]
+            user: [...this.state.user, this.state.ciao],
+            disable: true,
         })
         // console.log(this.state.user)
     }
     render() {
         return (
             <div>
+                <input name="in" type="text" disabled={this.state.disable} onChange={this.handleChange} value={this.state.ciao} />
+                <input name="genera" type="submit" onClick={this.handleGen} value="invia" />
                 <ul>
 
-                {this.state.user.map((el, index) =>
-                (<li key={index}>{el}</li>)
-                )}
+                    {this.state.user.map((el, index) =>
+                        (<li key={index}>{el}</li>)
+                    )}
                 </ul>
 
-                <input name="in" type="text" onChange={this.handleChange} value={this.state.ciao} />
-                <input name="genera" type="submit" onClick={this.handleGen} value="invia" />
             </div>
         )
     }
