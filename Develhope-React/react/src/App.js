232381fa15e9d5a7b08238c7d5e1396d46a4becf
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-06-20 19:48:27 +0200

    react

--- Develhope-React/react/src/App.js
+++ Develhope-React/react/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import InteractiveWelcome from './InteractiveWelcome ';
-import Counter from "./Counter";
+import {Counter} from "./Counter";
 import ClickTracker from './ClickTracker';
 import Login from './Login';
 import UncontrolledLogin from './UncontrolledLogin'
@@ -21,10 +21,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div />
-        <button onClick={handleShow}>toggleCounter</button>
+        {/* <button onClick={handleShow}>toggleCounter</button>
         {showCounter && <Counter />}  
-        <GithubUser />
+        <GithubUser /> */}
+        <Counter />
       </header>
     </div>
   );
