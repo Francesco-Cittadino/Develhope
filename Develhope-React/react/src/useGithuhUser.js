11ff5df4fb87e285877856d613efd1eac6be77cc
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-06-22 17:05:24 +0200

    react

--- Develhope-React/react/src/useGithuhUser.js
+++ Develhope-React/react/src/useGithuhUser.js
@@ -4,8 +4,9 @@ import useSWR from 'swr'
 const fetcher = url=>fetch(url).then((el)=>el.json())
 
 export function useGithubUser(username) {
-    const {data, error} = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    const {data, error} = useSWR( username ? `https://api.github.com/users/${username}`:null, fetcher)
     return{data, error, loading:!data && !error};
+    
     // const [data, setData] = useState(null)
     // const [loading, setLoading] = useState(false)
     // const [error, setError] = useState(null)
