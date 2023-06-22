35489230aa5ac23beb9d7d14766ff43494265e49
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-06-22 16:20:20 +0200

    react

--- Develhope-React/react/src/App.js
+++ Develhope-React/react/src/App.js
@@ -4,28 +4,39 @@ import { ShowGithubUser } from './ShowGithubUser'
 import { Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Route } from "react-router-dom";
-import {CounterUno} from "./CountUno";
+import { CounterUno } from "./CountUno";
 import Welcome from "./Welcome";
 
 function App() {
-// useEffect(()=>{
-//   fetch('https://api.github.com/users/Francesco-Cittadino')
-//   .then((el)=>el.json())
-//   .then((el)=>console.log(el))
+  // useEffect(()=>{
+  //   fetch('https://api.github.com/users/Francesco-Cittadino')
+  //   .then((el)=>el.json())
+  //   .then((el)=>console.log(el))
 
-// },[])
+  // },[])
   return (
     <div>
       <div>
-        <Link to='/'>home</Link>
-        <Link to='/Counter'>Counter</Link>
-        <Link to='/Welcome'>Welcome</Link>
-
+        <div>
+          <Link to='/'>home</Link>
+        </div>
+        <div>
+          <Link to='/Counter'>Counter</Link>
+        </div>
+        <div>
+          <Link to='/Welcome'>Welcome</Link>
+        </div>
       </div>
       <Routes>
-
-        <Route path='/Welcome' element={<Welcome />}/>
-        <Route path='/Counter' element={<CounterUno />}/>
+        <Route path='*' element={
+          <div><Link to='/'>Indietro</Link>
+            <p>
+              Not Found
+            </p>
+          </div>} />
+        <Route path='/' element={<CounterUno />} />
+        <Route path='/Welcome' element={<Welcome />} />
+        <Route path='/Counter' element={<CounterUno />} />
         <Route path='/users/:username' element={<ShowGithubUser />} />
       </Routes>
     </div>
