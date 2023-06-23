0547f30446f0ed0bc26bdf0987c275f8dd7e7e85
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-06-23 12:19:37 +0200

    ts1

--- Develhope-React/react/src/CarDetails.js
+++ Develhope-React/react/src/CarDetails.js
@@ -1,39 +1,43 @@
-import {useState, useRef} from 'react';
+import {useRef, useEffect} from 'react';
 
 // Create a CarDetails uncontrolled form that uses the HTML Form API to allow the user to insert details about a car, 
 // such as the model, the year and the color. Allow the form to 
 // receive a initialData prop that contains the default values of each input, 
 // and reset the form every time the initialData value changes.
 
-export function CarDetails({ initialData }) {
+export function CarDetails({ initialData = {model:"ford", year:1992, color:"white"} }) {
   
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-
-  };
-
   const refFormModel = useRef('')
   const refFormYear = useRef('')
   const refFormColor = useRef('')
-//   console.log(refForm.current)
 
-  return (
+  useEffect(()=>{
+    refFormModel.current.value = initialData.model
+    refFormYear.current.value = initialData.year
+    refFormColor.current.value = initialData.color
+  },[initialData])
+//   console.log(refForm.current)
+function handleChange (){
+  refFormModel.current.value = ""  
+  refFormYear.current.value = ""  
+  refFormColor.current.value = ""}
+  
+return (
     <form> 
       <label> Model:
         <input
-          type="text" name="model" ref={refFormModel} onChange={handleInputChange} />
+          type="text" name="model" ref={refFormModel} onChange={handleChange} />
       </label>
       
       <label>
         Year:
-        <input type="text" name="year" ref={refFormYear} onChange={handleInputChange} />
+        <input type="text" name="year" ref={refFormYear} onChange={handleChange} />
       </label>
 
       <label>
         Color:
         <input
-          type="text" name="color" ref={refFormColor} onChange={handleInputChange} />
+          type="text" name="color" ref={refFormColor} onChange={handleChange} />
       </label>
     </form>
   );
