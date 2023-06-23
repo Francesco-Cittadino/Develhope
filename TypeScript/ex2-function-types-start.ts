ef610e6ec8786f9b315fc8fb4fc7700776be9c8b
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-06-23 12:45:25 +0200

    ts2

--- TypeScript/ex2-function-types-start.ts
+++ TypeScript/ex2-function-types-start.ts
@@ -4,7 +4,7 @@
 
 // Add types to this function declaration.
 
-function doubleThePopulation(value) {
+function doubleThePopulation(value:number) {
     return value * 2;
 }
 
@@ -12,19 +12,24 @@ function doubleThePopulation(value) {
 
 doubleThePopulation(5);
 
-doubleThePopulation("6.12");
+// doubleThePopulation("6.12");
+doubleThePopulation(6.12);
 
 doubleThePopulation(8_526);
 
-doubleThePopulation(false);
+// doubleThePopulation(false);
+doubleThePopulation(0);
+
 
 // Alter this function so the `language2` parameter is optional.
 // Hint: Check `language2` is not `undefined` before passing it to `console.log()`.
 
-function languagesSpoken(country: string, language1: string, language2: string): void {
+function languagesSpoken(country: string, language1: string, language2?: string): void {
     console.log(`The languages spoken in ${country} are:`);
 
-    console.log(language1);
+    if(typeof language2 === "string"){
+        console.log(language1);
+    }
 
     console.log(language2);
 }
