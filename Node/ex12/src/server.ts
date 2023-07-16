2565078dd26f31895ecd3e9bcee4f8c9dbe40a64
Francesco Cittadino <francesco.cittadino2812@gmail.com>
2023-07-16 23:54:50 +0200

    ex12 node

---
+++ Node/ex12/src/server.ts
@@ -0,0 +1,22 @@
+import express from "express"
+import "express-async-errors";
+import morgan from "morgan";
+import { getAll, getOneByid, create, updateByid, deleteById } from "./component/planet"
+const app = express()
+const port = 3000
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get('/api/planet', getAll )
+
+app.get('/api/planet:id', getOneByid )
+
+app.post('/api/planet:id', create )
+
+app.put('/api/planet:id', updateByid )
+
+app.delete('/api/planet:id', deleteById)
+
+
+app.listen(port)
